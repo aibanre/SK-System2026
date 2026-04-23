da11afe5ff64dcf7d94564aa11ed37777515ebaf
aibanre <ivanrocat13@gmail.com>
2026-04-24 07:43:11 +0800

    Add UI layouts for all SK System modules

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SK_System
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void PasswordBox_PasswordChanged(object sender, RoutedEventArgs e)
+        {
+            if (txtPasswordPlaceholder != null)
+            {
+                txtPasswordPlaceholder.Visibility = string.IsNullOrEmpty(pwdPassword.Password) ? Visibility.Visible : Visibility.Collapsed;
+            }
+        }
+    }
+}
